15622f01e95ce60c979bfb6aa7fc76b4273926b6
kaczormp4 <kaczkaoryginal@gmail.com>
2021-01-05 13:26:50 +0100

    zad3

---
+++ 3.Operatory/script.js
@@ -0,0 +1,57 @@
+//zad 1
+
+let bool1 = true;
+let bool2 = false;
+
+console.log(bool1 == bool2);
+
+//zad 2
+
+let num1=1;
+let num2=2;
+
+let moduloResult=0;
+
+moduloResult=num1%num2;
+
+console.log(moduloResult);
+
+//zad 3
+
+let txt1 = "abc";
+let txt2 = "xyz";
+let stringsResult = "";
+
+stringsResult = txt1+txt2;
+
+console.log(stringsResult);
+
+//zad4
+
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber==someString);
+// true - gdyz wartośc jest taka sama a zmienne porównujemy "=="
+console.log(someNumber===someString);
+// false - gdyz wartośc jest taka sama ale typ zmiennych inny -> zmienne porównujemy "==="
+
+//zad 5
+
+let counter = 30;
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+//zad 6
+let num1 = 1;
+let num2 = 2;
+let result = null;
+result = num1 > num2;
+console.log(result);
+
+
+
+
+//Bartłomiej Kaczmarczyk 12280
