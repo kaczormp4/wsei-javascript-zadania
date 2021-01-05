9ce939a54ef5382f699e6834608c5a2f2ae74ecd
kaczormp4 <kaczkaoryginal@gmail.com>
2021-01-05 13:07:12 +0100

    zad1.1

---
+++ 1.Początki/script.js
@@ -0,0 +1,20 @@
+//zad 1
+console.log("Bartłomiej");
+
+
+//zad 2
+console.log(`12 + 32 = ${12 + 32}`);
+console.log(`52 / 16 = ${52 / 16}`);
+console.log(`5 * 2 = ${5 * 2}`);
+
+//zad 3
+console.log("Hello World");
+//wyświetla string "Hello World"
+
+console.log(Hello World);
+//wyświetla Syntax error - brak znaków "
+
+console.log("Hello World);
+// wyświetla Syntax error - brak znaku "
+
+//Bartłomiej Kaczmarczyk 12280
