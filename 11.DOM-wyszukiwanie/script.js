1b50d010c5e513e2bc1552a1a027c7ba42e29001
kaczormp4 <kaczkaoryginal@gmail.com>
2021-01-12 17:06:46 +0100

    11. DOM got

---
+++ 11.DOM-wyszukiwanie/script.js
@@ -0,0 +1,47 @@
+//zad 1
+
+let links = document.getElementsByClassName('list');
+
+console.log(links);
+
+//zad 2
+
+function tagElements(tag) {
+    const elements = document.getElementsByTagName(tag);
+    for (let i = 0; i<elements.length; i++) {
+        console.log(elements[i]);
+    }
+}
+
+tagElements('ul');
+
+
+//zad 3
+
+const listElement = document.getElementById('list');
+
+console.log(listElement);
+
+
+
+//zad 4
+
+
+function elements(element) {
+    console.log(element);
+}
+
+elements(document.querySelectorAll("li"));
+elements(document.querySelectorAll("ul"));
+elements(document.querySelectorAll("span"));
+elements(document.querySelectorAll("div.list span"));
+elements(document.querySelectorAll("div#spans span"));
+
+
+
+
+
+
+
+
+
